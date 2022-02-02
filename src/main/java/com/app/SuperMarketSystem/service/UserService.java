cdfd27292f774b93c2d3f2e39cf622b2f26b181d
mfida <mfida90@gmail.com>
2022-02-02 18:33:18 +0000

    Complete APIs; wrote some tests.

--- src/main/java/com/app/SuperMarketSystem/service/UserService.java
+++ src/main/java/com/app/SuperMarketSystem/service/UserService.java
@@ -1,8 +1,10 @@
 package com.app.SuperMarketSystem.service;
 
 import com.app.SuperMarketSystem.dto.ApiResponse;
+import com.app.SuperMarketSystem.model.Order;
 import com.app.SuperMarketSystem.model.User;
 import com.app.SuperMarketSystem.repository.UserRepository;
+import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -19,7 +21,7 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public ApiResponse findAllUsers() {
+/*    public ApiResponse findAllUsers() {
         ApiResponse apiResponse = new ApiResponse();
         try {
             List<User> userList = userRepository.findAll();
@@ -37,6 +39,15 @@ public class UserService {
             apiResponse.setMessage(e.getMessage());
             apiResponse.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
             return apiResponse;
+        }
+    }*/
+
+    public List<User> findAllUsers() {
+        try {
+            List<User> userList = userRepository.findAll();
+            return userList;
+        } catch (Exception e) {
+            throw new RuntimeException("There are no users in the database");
         }
     }
 
@@ -60,6 +71,7 @@ public class UserService {
         try {
             Optional<User> user = userRepository.findById(userId);
             if (user.isPresent()) {
+                user.get().setOrders(null);
                 userRepository.delete(user.get());
                 apiResponse.setStatus(HttpStatus.OK.value());
                 apiResponse.setMessage("Successfully deleted the user from the database");
@@ -99,7 +111,7 @@ public class UserService {
         }
     }
 
-    public ApiResponse getUserById(Integer userId) {
+    /*public ApiResponse getUserById(Integer userId) {
         ApiResponse apiResponse = new ApiResponse();
         try {
             Optional<User> user = userRepository.findById(userId);
@@ -111,6 +123,44 @@ public class UserService {
                 apiResponse.setData(null);
                 apiResponse.setStatus(HttpStatus.NOT_FOUND.value());
                 apiResponse.setMessage("There is no user in the database");
+            }
+            return apiResponse;
+        } catch (Exception e) {
+            apiResponse.setMessage(e.getMessage());
+            apiResponse.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+            return apiResponse;
+        }
+    }*/
+    public User getUserById(Integer userId) {
+        try {
+            User user = userRepository.getById(userId);
+            if (null != user) {
+                return user;
+            } else {
+                throw new NotFoundException("User not found");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    public ApiResponse getOrdersByUserId(Integer userId) {
+        ApiResponse apiResponse = new ApiResponse();
+        try {
+            Optional<User> user = userRepository.findById(userId);
+            if (user.isPresent()) {
+                List<Order> orders = user.get().getOrders();
+                if (orders.isEmpty()) {
+                    apiResponse.setMessage("There are no orders made yet by this user");
+                } else {
+                    apiResponse.setMessage("These are the orders that this user made");
+                }
+                apiResponse.setStatus(HttpStatus.OK.value());
+                apiResponse.setData(orders);
+            } else {
+                apiResponse.setData(null);
+                apiResponse.setStatus(HttpStatus.NOT_FOUND.value());
+                apiResponse.setMessage("There is no user against this id in the database");
             }
             return apiResponse;
         } catch (Exception e) {
--- src/test/java/com/app/SuperMarketSystem/SuperMarketSystemApplicationTests.java
+++ src/test/java/com/app/SuperMarketSystem/SuperMarketSystemApplicationTests.java
@@ -1,13 +1,45 @@
 package com.app.SuperMarketSystem;
 
+import com.app.SuperMarketSystem.model.User;
+import com.app.SuperMarketSystem.repository.UserRepository;
+import com.app.SuperMarketSystem.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class SuperMarketSystemApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private UserService userService;
+    @MockBean
+    private UserRepository userRepository;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    public void getUsersTest() {
+        when(userRepository.findAll()).thenReturn(Stream.of(new User(1, "Test", "User", "test@gmail.com", 20, "password", "address", "phone number")).collect(Collectors.toList()));
+        assertEquals(1, userService.findAllUsers().size());
+    }
+    @Test
+    public void getUsersById(){
+        Integer id = 1;
+        User user = new User(1, "Test", "User", "test@gmail.com", 20, "password", "address", "phone number");
+        when(userRepository.getById(id)).thenReturn(user);
+        assertEquals(user, userService.getUserById(id));
+    }
 
 }
